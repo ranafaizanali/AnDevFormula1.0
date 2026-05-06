1f3d9d729f8e39ce707ead322ece7db27304fd64
ranafaizanali <itxfaizi38@gmail.com>
2026-05-06 21:05:17 +0500

    Updated (Class 17)!

--- app/src/main/java/com/example/andevformula/MainActivity.kt
+++ app/src/main/java/com/example/andevformula/MainActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.andevformula.s7_scaffold.BottomBarExample
-import com.example.andevformula.s7_scaffold.FABExample
-import com.example.andevformula.s7_scaffold.ScaffoldExample
-import com.example.andevformula.s7_scaffold.TopBarExample
-import com.example.andevformula.s8_lists.ItemsExample
-import com.example.andevformula.s8_lists.LazyColumnExample
-import com.example.andevformula.s8_lists.LazyRowExample
+import com.example.andevformula.s8_lists.LazyHorizontalGridExample
+import com.example.andevformula.s8_lists.LazyVerticalGridExample
 import com.example.andevformula.s8_lists.MixedLayoutsExample
+import com.example.andevformula.s9_state_management.ParentComposable
+import com.example.andevformula.s9_state_management.SaveStateExample
+import com.example.andevformula.s9_state_management.StateManagementExample
 import com.example.andevformula.ui.theme.AnDevFormulaTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +19,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AnDevFormulaTheme {
 
-                MixedLayoutsExample()
+                ParentComposable()
 
             }
         }
